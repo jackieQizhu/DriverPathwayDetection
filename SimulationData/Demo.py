eda2f270760ea7a1a8b0dcb928ea9e01f056fb56
Serendipity <Aa53624931@163.com>
2022-02-03 01:17:58 +0800

    a little modification

--- SimulationData/Demo.py
+++ SimulationData/Demo.py
@@ -12,44 +12,50 @@ Mutation_Rate = 0.3
 ChromosomeX = np.arange(MAXG_G)
 POP_ONE = []
 POP_TWO = []
-COOLPERATION = []
+COOPERATION = []
 
 
 # define fitness function
 def Fit(x):
     return W(M[i])
 
-def pop_and_coolperation(pop): # define the initial value
+def initial_pop_and_cooperation(pop): # define the initial value
     # POP_ONE = pop[0:POP_SIZE/2, 0:K]
     # POP_TWO = pop[POP_SIZE/2:POP_SIZE, K:]
     POP_ONE[0] = pop[random.randint(POP_SIZE)]
     POP_TWO[0] = pop[random.randint(POP_SIZE)]
-    COOLPERATION[0] = (POP_ONE[0] | POP_TWO[0])
+    COOPERATION[0] = (POP_ONE[0] | POP_TWO[0])
 
-def selection(POP_ONE, POP_TWO):
-    new_pop = []
-    for father in POP_ONE:
-        child = father
-        mother = POP_TWO[np.random.randint(POP_SIZE)]
-        for i in father:
-            for j in mother:
-                if i == j:
-                    selector = father.remove(i) | mother.remove(j)
-
-        child[cross_points:] = mother[cross_points:]  # 孩子得到位于交叉点后的母亲的基因
-        mutation(child)  # 每个后代有一定的机率发生变异
-        new_pop.append(child)
-
-    return new_pop
 def get_fitness(pop):
 
     w
+
+def pop_selection(POP_ONE, POP_TWO):
+    SAME = [x for x in POP_ONE[gen] if x in POP_TWO[gen]]
+    REST = [y for y in (POP_ONE[gen] + POP_TWO[gen]) if y not in SAME]
+    REST = random.stuffle(REST)
+    Rest_pre = REST[0: len(REST) / 2]
+    Rest_back = REST[len(REST) / 2:]
+    POP_ONE[gen + 1] = SAME.extend(Rest_pre)
+    POP_TWO[gen + 1] = SAME.extend(Rest_back)
+    return POP_ONE[gen + 1], POP_TWO[gen + 1]
+
+def cooperation_selection():
+    SAME = [x for x in POP_ONE[gen] if x in POP_TWO[gen]]
+    REST = [y for y in (POP_ONE[gen] + POP_TWO[gen]) if y not in SAME]
+    REST = random.stuffle(REST)
+    Rest_pre = REST[0: len(REST) / 2]
+    Rest_back = REST[len(REST) / 2:]
+    POP_ONE[gen + 1] = SAME.extend(Rest_pre)
+    POP_TWO[gen + 1] = SAME.extend(Rest_back)
+    return POP_ONE[gen + 1], POP_TWO[gen + 1]
+
 if __name__ == "__main__":
     K = int(input())
     # Generate Initial_chromosome
     Init_chromosome = random.shuffle(ChromosomeX)[K]
     pop = [[Init_chromosome] for i in POP_SIZE]
-    pop_and_coolperation(pop)
+    initial_pop_and_cooperation(pop)
     # pop = random.randint(0, MAXG_G, size = (POP_SIZE, K))
     for g in MAXG_G:
         # set the object as a iterator
@@ -59,11 +65,20 @@ if __name__ == "__main__":
             t = 0
             if gen < MAXG_G[g] and t < MAXT:
                 # begin to iterate
-                # step four : selection operation tp pop
-                SAME = [x for x in POP_ONE[gen] if x in POP_TWO[gen]]
-                REST = [y for y in (POP_ONE[gen] + POP_TWO[gen]) if y not in SAME]
-                REST = random.stuffle(REST)
-                Rest_pre = REST[0: len(REST)/2]
-                Rest_back = REST[len(REST)/2:]
-                POP_ONE[gen + 1] = SAME.extend(Rest_pre)
-                POP_TWO[gen + 1] = SAME.extend(Rest_back)
+                # step fourth : selection operation in pop
+                pop_selection(POP_ONE, POP_TWO)
+                # step fifth : selection operation in cooperation
+                cooperation_selection()
+                # step sixth :
+                # step seventh :
+                # step eighth :
+                # step ninth :
+                # step tenth :
+                # step eleventh :
+                # step twelfth :
+                # step thirteenth :
+                # step fourteenth :
+                # step fifteenth :
+                # step sixteenth :
+                # step seventeenth :
+
